b485f6250fd745ce8192682e86f0c8ab55079de6
K4maS <107948849+K4maS@users.noreply.github.com>
2023-01-25 10:35:14 +0500

    deploy

---
+++ timer.js
@@ -0,0 +1,47 @@
+
+(() => {
+
+    // функция таймера
+    function setTimer(time, timerElement) {
+
+        let firstOperation = true;
+        let minute = 59;
+        let showMinute = '';
+        let operatingTime = 0;
+
+        if (firstOperation) {
+            operatingTime = time;
+        }
+
+        let timer = setInterval(() => {
+            minute -= 1;
+            firstOperation = false;
+            if (minute < 0) {
+                operatingTime -= 1;
+                minute = 59;
+            }
+            if (operatingTime < 0) {
+                operatingTime = time;
+                firstOperation = true;
+            }
+            if (String(minute).length == 1) {
+                showMinute = '0' + minute;
+            }
+            else {
+                showMinute = minute;
+            }
+
+            timerElement.textContent = operatingTime + ':' + showMinute;
+        }, 1000);
+
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        let timerElement = document.getElementById('timer');
+        let halfHour = 29;
+        
+        setTimer(halfHour, timerElement);
+    });
+
+
+})();
